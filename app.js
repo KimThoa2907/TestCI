9dacb09a12d0231d347f80385bd7c9f4dd0cc412
LAPTOP-MTGPP3N7\Admin <phanthikimthoa.ltp@gmail.com>
2023-04-09 21:17:12 +0700

    Them unit test

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-console.log("1");
-console.log("2");
+const{add, minus} = require('./operator');
+const util = require('util');
+var a=10;
+var b=5;
+console.log(util.format('%d+%d=%d',a,b,add(a,b)));
+console.log(util.format('%d-%d=%d',a,b,minus(a,b)));
+
---
+++ operator.js
@@ -0,0 +1,23 @@
+
+
+function add(a,b){
+    a = parseInt(a);
+    b= parseInt(b);
+    let c= a+b;
+    if(isNaN(c)){
+        throw new error('Input error');
+    }
+    return c;
+}
+function minus(a,b){
+    a = parseInt(a);
+    b= parseInt(b);
+    let c= a-b;
+    if(isNaN(c)){
+        throw new error('Input error');
+    }
+    return c;
+}
+module.exports={
+    add,minus
+}
